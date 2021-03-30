283570275e2703e02d025fe9ece64ce8bf13edaa
gantoin <m.gauthier.antoine@gmail.com>
2021-03-30 18:50:12 +0200

    Add UI

--- src/main/java/fr/gantoin/bitchuteuploader/api/UploadApi.java
+++ src/main/java/fr/gantoin/bitchuteuploader/api/UploadApi.java
@@ -2,10 +2,14 @@ package fr.gantoin.bitchuteuploader.api;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.github.kiulian.downloader.YoutubeException;
 
@@ -17,7 +21,7 @@ import fr.gantoin.bitchuteuploader.service.YoutubeDownloaderService;
 import fr.gantoin.bitchuteuploader.service.mapper.BitchuteUploadMapper;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class UploadApi {
 
@@ -25,18 +29,44 @@ public class UploadApi {
 
     private final YoutubeDownloaderService youtubeDownloaderService;
 
-    @PostMapping("/upload")
+    @GetMapping("/")
+    public String uploadFrom(Model model) {
+        model.addAttribute("upload", new BitchuteUploadDto());
+        return "upload";
+    }
+
+    @PostMapping("/upload/without-ui")
     public void upload(@RequestBody BitchuteUploadDto dto) {
-        try {
-            WebDriverManager chromedriver = WebDriverManager.chromedriver();
-            chromedriver.setup();
-            BitchuteVideo downloadedVideo = youtubeDownloaderService.download(dto.getYoutubePath());
-            bitchuteUploadMapper.map(chromedriver, dto).uploadVideo(downloadedVideo);
-        } catch (YoutubeException | IOException e) {
-            e.printStackTrace();
-        } finally {
-            deleteFolder(new File("my_videos"));
-        }
+        CompletableFuture.runAsync(() -> {
+            try {
+                WebDriverManager chromedriver = WebDriverManager.chromedriver();
+                chromedriver.setup();
+                BitchuteVideo downloadedVideo = youtubeDownloaderService.download(dto.getYoutubePath());
+                bitchuteUploadMapper.map(chromedriver, dto).uploadVideo(downloadedVideo);
+            } catch (YoutubeException | IOException e) {
+                e.printStackTrace();
+            } finally {
+                deleteFolder(new File("my_videos"));
+            }
+        });
+    }
+
+    @PostMapping("/upload")
+    public String upload(@ModelAttribute BitchuteUploadDto dto, Model model) {
+        CompletableFuture.runAsync(() -> {
+            model.addAttribute("upload", dto);
+            try {
+                WebDriverManager chromedriver = WebDriverManager.chromedriver();
+                chromedriver.setup();
+                BitchuteVideo downloadedVideo = youtubeDownloaderService.download(dto.getYoutubePath());
+                bitchuteUploadMapper.map(chromedriver, dto).uploadVideo(downloadedVideo);
+            } catch (YoutubeException | IOException e) {
+                e.printStackTrace();
+            } finally {
+                deleteFolder(new File("my_videos"));
+            }
+        });
+        return "result";
     }
 
     public static void deleteFolder(File folder) {
